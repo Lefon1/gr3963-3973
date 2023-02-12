f7f1b2758676cd706b20fd818b23261a1d501c35
lefon <1504lefon@gmail.com>
2023-02-12 18:58:11 +0300

    Разбираем решения от Одногруппника

--- ZadachkaOtvladimira/Program.cs
+++ ZadachkaOtvladimira/Program.cs
@@ -1,12 +1,50 @@
-﻿Console.Clear();
-                Console.Write("Введите первое число: ");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите второе число: ");
-                int number2 = Convert.ToInt32(Console.ReadLine());
+﻿// Console.Clear();
+//                 Console.Write("Введите первое число: ");
+//                 int number1 = Convert.ToInt32(Console.ReadLine());
+//                 Console.Write("Введите второе число: ");
+//                 int number2 = Convert.ToInt32(Console.ReadLine());
 
-                int number = Math.Abs(number1);
+//                 int number = Math.Abs(number1);
 
-                for (int i = 0; i < number; i++)
-                {
-                    Console.Write($"Остаток от деления {i} на {number2} = {i% number2}\n");
-                }
+//                 for (int i = 0; i < number; i++)
+//                 {
+//                     Console.Write($"Остаток от деления {i} на {number2} = {i% number2}\n");
+//                 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Напишите программу, которая на вход принимает число и выдаёт, 
+        // является ли число чётным (делится ли оно на два без остатка).
+
+        // Console.Clear();
+        // Console.Write("Введите первое число: ");
+        // int num = Convert.ToInt32(Console.ReadLine());
+
+        // switch (num % 2)
+        // {
+        //     case 0:
+        //         Console.WriteLine($"Число: {num} - четное");
+        //         break;
+        //     case 1:
+        //         Console.WriteLine($"Число: {num} - нечётное");
+        //         break;
+        // }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Напишите программу, которая на вход принимает число и выдаёт, 
+ // является ли число чётным (делится ли оно на два без остатка).
+
+        Console.Clear();
+        Console.Write("Введите первое число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        switch (num % 2)
+        {
+            case 0:
+                Console.WriteLine($"Число: {num} - четное");
+                break;
+            case 1:
+                Console.WriteLine($"Число: {num} - нечётное");
+                break;
+        }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
